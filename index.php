a5b2a55da7c5c9b683995928e87767ffe1a6195f
daniella0916 <76413721+daniella0916@users.noreply.github.com>
2020-12-21 09:40:22 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<style>
+* {
+	box-sizing: border-box;
+}
+h1{
+	text-align: center;
+}
+.outer-grid{
+	display: flex;
+	flex-wrap: wrap;
+	padding: 0 4px;
+}
+.inner-grid{
+	flex: 25%;
+	max-width: 25%;
+	padding: 0 0px;
+	text-align: center;
+	margin-left: 6%;
+}
+.inner-grid img{
+	margin-top: 8px;
+	width: 100%;
+	padding: 10px;
+}
+@media screen and (max-width: 800px){
+	.inner-grid{
+		flex: 50%;
+		max-width: 50%;
+	}
+}
+@media screen and (max-width: 600px){
+	.inner-grid{
+		flex: 100%;
+		max-width: 100%;
+	}
+}
+</style>
+<body>
+<h1>My Plants Photo Gallery</h1>
+<div class ="outer-grid">
+<div class ="inner-grid">
+<a href ="Album.html"><img src="college2.jpg"></a>
+<h1>My High School Days</h1>
+<img src="hs1.jpg">
+<img src="hs2.jpg">
+</div>
+
+
+<div class="inner-grid">
+<h1>My Senior High School Days</h1>
+<img src="senior1.jpg"/></a>
+<img src="senior2.jpg"/></a>
+<img src="senior3.jpg"/></a>
+</div>
+
+<div class="inner-grid">
+<h1>My College Days</h1>
+<img src="college1.jpg"></a>
+<img src="college2.jpg">
+<img src="college3.jpg">
+</div>
+</div>
+</body>
+</html>
